ebd22cda4d4cb51de47c827283c39a3c6dde422b
Sorokovsky <Sorokovskys@ukr.net>
2026-07-10 16:36:56 +0300

    feature: add tanstack query

--- src/app/providers/index.ts
+++ src/app/providers/index.ts
@@ -2,9 +2,13 @@ import { EnvironmentProviders, Provider } from "@angular/core";
 import { STORAGE_PROVIDER } from "@shared/index";
 import { ROUTER_PROVIDER } from "./router.provider";
 import { GLOBAL_ERROR_LISTENERS_PROVIDER } from "./global-error-listeners.provider";
+import { HTTP_PROVIDER } from "./http.provider";
+import { QUERY_PROVIDER } from "./query.provider";
 
-export const PROVIDERS: (Provider | EnvironmentProviders)[] = [
+export const PROVIDERS: (Provider | EnvironmentProviders | Provider[])[] = [
   GLOBAL_ERROR_LISTENERS_PROVIDER,
   ROUTER_PROVIDER,
   STORAGE_PROVIDER,
+  HTTP_PROVIDER,
+  QUERY_PROVIDER,
 ];
---
+++ src/app/providers/query.provider.ts
@@ -0,0 +1,5 @@
+import { Provider } from "@angular/core";
+import { provideTanStackQuery, QueryClient } from "@tanstack/angular-query-experimental";
+import { withDevtools } from "@tanstack/angular-query-experimental/devtools";
+
+export const QUERY_PROVIDER: Provider[] = provideTanStackQuery(new QueryClient(), withDevtools());
